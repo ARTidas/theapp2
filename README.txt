52fdec0285fff8bd6be3f112fe696381b9be3ba3
Veres Zoltán <artidas@artidas.hu>
2022-09-26 21:45:25 +0200

    Initialize...

--- README.txt
+++ README.txt
@@ -1,1 +1,17 @@
 Readme...
+
+Command you going to need to develop this App:
+curl -sL https://aws-amplify.github.io/amplify-cli/install-win -o install.cmd && install.cmd
+amplify pull --appId d2z62jb76jidsg --envName staging
+
+amplify pull
+
+
+https://radixweb.com/blog/installing-npm-and-nodejs-on-windows-and-mac
+
+npm install -S aws-amplify
+npm install -g npm@8.19.2
+
+
+npm start
+npm run
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "theapp": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3theappstoragef232ddc9": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
